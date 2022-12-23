ea2a204c85fa940d9a854c6c9a139bb5e34e203b
Jakub Oczkowski <jakuboczkowski2@gmail.com>
2022-12-23 13:12:51 +0100

    Refactor login handler

--- auth/login.go
+++ auth/login.go
@@ -26,46 +26,86 @@ func CheckPasswordHash(password, hash string) bool {
 	return err == nil
 }
 
+type postLoginAction struct {
+	w http.ResponseWriter
+	r *http.Request
+	db *gorm.DB
+	deseralizedParams credentails
+	tokenString string
+	expirationTime time.Time
+}
+
+func (self *postLoginAction) handle()  {
+	if !self.deserializeParams() { return }
+	if !self.validateParams() { return }
+	if !self.auth() { return }
+
+	http.SetCookie(self.w, &http.Cookie{
+		Name:    "token",
+		Value:   self.tokenString,
+		Expires: self.expirationTime,
+	})
+}
+
+func (self *postLoginAction) deserializeParams() bool {
+	err := json.NewDecoder(self.r.Body).Decode(&self.deseralizedParams)
+	if err != nil {
+		self.w.WriteHeader(http.StatusBadRequest)
+		return false
+	}
+
+	return true
+}
+
+func (self *postLoginAction) validateParams() bool {
+	if self.deseralizedParams.Email == "" || self.deseralizedParams.Password == "" {
+		self.w.WriteHeader(http.StatusBadRequest)
+		return false
+	}
+
+	return true
+}
+
+func (self *postLoginAction) auth() bool {
+	var user User
+
+	result := self.db.First(user, "email = ?", self.deseralizedParams.Email)
+
+	if result.RowsAffected == 0 {
+		self.w.WriteHeader(http.StatusUnauthorized)
+		return false
+	}
+	
+	if !CheckPasswordHash(self.deseralizedParams.Password, user.HashedPassword) {
+		self.w.WriteHeader(http.StatusUnauthorized)
+		return false
+	}
+
+	self.expirationTime = time.Now().Add(5 * time.Minute)
+	claims := &claims{
+		Email: self.deseralizedParams.Email,
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: self.expirationTime.Unix(),
+		},
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	var err error
+	self.tokenString, err = token.SignedString(jwtKey)
+	if err != nil {
+		self.w.WriteHeader(http.StatusInternalServerError)
+		return false
+	}
+
+	return true
+}
+
 func loginHandler(db *gorm.DB) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		var credentails credentails
-
 		switch r.Method {
 		case http.MethodPost:
-			err := json.NewDecoder(r.Body).Decode(&credentails)
-			if err != nil {
-				w.WriteHeader(http.StatusBadRequest)
-				return
-			}
-
-			expectedPassword, ok := users[credentails.Email]
-
-			if !ok || expectedPassword != credentails.Password {
-				w.WriteHeader(http.StatusUnauthorized)
-				return
-			}
-
-			expirationTime := time.Now().Add(5 * time.Minute)
-			claims := &claims{
-				Email: credentails.Email,
-				StandardClaims: jwt.StandardClaims{
-					ExpiresAt: expirationTime.Unix(),
-				},
-			}
-
-			token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
-			tokenString, err := token.SignedString(jwtKey)
-			if err != nil {
-				w.WriteHeader(http.StatusInternalServerError)
-				return
-			}
-
-			http.SetCookie(w, &http.Cookie{
-				Name:    "token",
-				Value:   tokenString,
-				Expires: expirationTime,
-			})
-
+			action := postLoginAction{w: w, r: r, db: db}
+			action.handle()
 		default:
 			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 		}
